f8f92fa8c28327b96bb6448f273434c4f0c5db01
Istvan Vizhanyo <vizhanyo.istvan0@gmail.com>
2024-02-09 11:49:44 +0000

    Added front-end user interface using template inheritance

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env  # noqa
+    import env  # noqa (no quality assurance)
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
@@ -10,4 +10,4 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
 
-from taskmanager import routes
+from taskmanager import routes # noqa (no quality assurance)
--- taskmanager/models.py
+++ taskmanager/models.py
@@ -4,8 +4,11 @@ from taskmanager import db
 class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
+        # __repr__ to represent itself in the form of a string
         return self.category_name
 
 
@@ -20,4 +23,6 @@ class Task(db.Model):
     category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
-        return self.category_name
+        return "#{0} - Task: {1} | Urgent: {2}".format(
+            self.id, self.task_name, self.is_urgent
+        )
